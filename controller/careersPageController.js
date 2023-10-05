ce4aebbe7b37dbad1f9112317f505f49cfefe82b
jyoth <jyotigiri.giri48@gmail.com>
2023-10-06 00:30:21 +0530

    changes

--- controller/careersPageController.js
+++ controller/careersPageController.js
@@ -1,18 +1,30 @@
 const axios = require('axios');
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
 let careersPage = async (req, res) => {
     try {
         let response = await getData();
-        console.log(JSON.stringify(response.data))
+        const dom = new JSDOM(response.data);
+        //console.log(dom.window.document.querySelectorAll(".department").textContent);
+        var els = dom.window.document.querySelectorAll('.department,.number-of-positions,.job-name');
+        els.forEach((el) => {
+            console.log(el.textContent,el.parentElement.textContent,el.childNodes.textContent);
+        });
+        //console.log(await response.data);
+        //const root = parse(response.data);
+        //console.log(root, response.data);
+        //const department = root.querySelector("div");
         res.status(200).send(response.data)
     } catch (error) {
         res.status(400).send(error)
     }
-    
+
 }
 
 let getData = async () => {
     try {
-        const response = await axios.get('https://jobs.lever.co/actian/');
+        const response = await axios.get('https://www.actian.com/company/careers/');
         return response;
         //console.log(response);
     } catch (error) {
